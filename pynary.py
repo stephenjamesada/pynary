06d865f1302e7bc1313dcc28a3de0a317f4d0fd1
stephenjamesada <ada_stephen@proton.me>
2025-11-12 16:56:04 -0600

    Add .gitignore, migrate calculator_mode() function in pynary.py to calculator_mode.py for modularity & code readability, add MIT LICENSE

--- pynary.py
+++ pynary.py
@@ -7,62 +7,11 @@ from rich.progress import Progress
 import pyfiglet
 import time as t
 import random as r
+import calculator_mode as cal_mode
 
 MAX_VALUE = 8
 BASE_2_VALUES = "01"
 err = "[bold red]Error[/bold red]"
-warn = "[bold yellow]Warning[/bold yellow]"
-
-def calculator_mode():
-    cm = "[bold](CM) [/bold]"
-
-    def get_binaries():
-        while True:
-            bin_num1 = str(Prompt.ask(f"[cyan]{cm}Enter binary number 1[/cyan]"))
-            num1_len = len(bin_num1)
-
-            bin_num2 = str(Prompt.ask(f"[cyan]{cm}Enter binary number 2[/cyan]"))
-            num2_len = len(bin_num2)
-
-            if num1_len > MAX_VALUE or num2_len > MAX_VALUE:
-                print(f"{err}: [red]8 digits maximum for both binary numbers.[/red]")
-            elif not all(char in BASE_2_VALUES for char in bin_num1) or not all(char in BASE_2_VALUES for char in bin_num2):
-                print(f"{err}: [red]Only binary 0 or 1 values are allowed for each number.[/red]")
-            else:
-                return bin_num1, bin_num2
-                break
-
-    bin_num1, bin_num2 = get_binaries()
-                
-    dec1 = int(bin_num1, 2)
-    dec2 = int(bin_num2, 2)
-
-    def get_operator():
-        while True:
-            operator = Prompt.ask(f"[cyan]{cm}Choose an operator[/cyan]", choices=["+", "-", "*", "/", "**"])
-            return operator
-            break
-                
-    operator = get_operator()
-
-    match operator:
-        case "+":
-            print(f"[green]{bin_num1} + {bin_num2} = {dec1 + dec2}[/green]")
-        case "-":
-            print(f"[green]{bin_num1} - {bin_num2} = {dec1 - dec2}[/green]")
-        case "*":
-            print(f"[green]{bin_num1} * {bin_num2} = {dec1 * dec2}[green]")
-        case "/":
-            if dec2 == 0:
-                print(f"{err}: [red]Cannot divide by zero.[/red]")
-            else:
-                print(f"[green]{bin_num1} / {bin_num2} = {dec1 / dec2}[/green]")
-        case "**":
-            print(f"[green]{dec1 ** dec2}[/green]")
-            if dec1 ** dec2 > 2**64:
-                print(f"{warn}: [yellow]Result is very large![/yellow]")
-        case _:
-            print(f"{err}: [red]Invalid operator.[/red]")
 
 def title():
     title_text = pyfiglet.figlet_format("Pynary", font="slant")
@@ -94,7 +43,7 @@ try:
 
             progress.stop()
         
-            calculator_mode()
+            cal_mode.calculator_mode()
             continue
 
         if length > MAX_VALUE:
